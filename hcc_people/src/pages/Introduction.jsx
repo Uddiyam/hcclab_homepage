a441ecf4216f5605ca4a57ab4d09279525b69d31
Suji Chae <63655826+suji8073@users.noreply.github.com>
2022-06-17 14:40:05 +0900

    Delete hcc_people directory

--- hcc_people/src/pages/Introduction.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Header from "../components/Header";
-
-function Introduction() {
-  return <Header />;
-}
-
-export default Introduction;
--- hcc_people/src/pages/Publications.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Header from "../components/Header";
-
-function Publications() {
-  return <Header />;
-}
-
-export default Publications;
