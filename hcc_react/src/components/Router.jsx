02566aba91a93c8a1ec9a9fc9ab3c40feb4b2945
iyu88 <iyu88887@gmail.com>
2022-06-20 18:30:18 +0900

    Edit Router & NPM

--- hcc_react/src/components/Router.jsx
+++ hcc_react/src/components/Router.jsx
@@ -1,24 +1,22 @@
 import Header from "./Header";
 import Home from "../pages/Home";
 import People from "../pages/People";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Lectures from "../pages/Lectures";
 import Publications from "../pages/Publications";
 import Introduction from "../pages/Introduction";
 
 const AppRouter = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <>
-          <Route exact path="/Home" element={<Home />} />
-          <Route exact path="/Introduction" element={<Introduction />} />
-          <Route exact path="/People" element={<People />} />
-          <Route exact path="/Lectures" element={<Lectures />} />
-          <Route exact path="/Publications" element={<Publications />} />
-        </>
+        <Route exact path="/Home" element={<Home />} />
+        <Route exact path="/Introduction" element={<Introduction />} />
+        <Route exact path="/People" element={<People />} />
+        <Route exact path="/Lectures" element={<Lectures />} />
+        <Route exact path="/Publications" element={<Publications />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
