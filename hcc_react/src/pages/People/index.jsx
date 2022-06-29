86de85d01df983fff9332df514c2ef75be4fd6a5
Uddiyam <97297404+Uddiyam@users.noreply.github.com>
2022-06-29 15:34:20 +0900

    수정

--- hcc_react/src/pages/People/index.jsx
+++ hcc_react/src/pages/People/index.jsx
@@ -163,72 +163,72 @@ export default function People() {
           <li>
             <b>학회 발표</b>
           </li>
-        </ul>
-        <ul className={styles.Lists}>
-          <li>
-            2022.04.28. 김현수, 곽소정, 정범영, 박규동.{" "}
-            <b>
-              벽걸이형 청각보조기기의 사용자 평가: 사용자 만족도 및 음질을
-              중심으로.
-            </b>{" "}
-            대한인간공학회 춘계학술대회
-          </li>
-          <li>
-            2022.02.10. 이재은, 이지은, 임예희, 박규동, 조민수.{" "}
-            <b>
-              자연어처리 기술을 활용한 유튜브 악성 댓글 자동 블라인드 시스템.
-            </b>{" "}
-            HCI Korea 2022.
-          </li>
-          <li>
-            2022.02.10. 권준혁, 소현수, 장석원, 손재성, 박규동.{" "}
-            <b>음악 커버 영상 제작을 위한 애플리케이션 설계 및 개발.</b> HCI
-            Korea 2022.
-          </li>
-          <li>
-            2021.11.29. 이영현, 김현수, 박규동.{" "}
-            <b>
-              게임화를 활용한 재활 운동의 연구 동향: 사용자의 동기부여를
-              중심으로.
-            </b>{" "}
-            대한인간공학회 추계학술대회
-          </li>
-          <li>
-            2021.10.20. 정범영, 박규동.{" "}
-            <b>
-              Heuristic Evaluation for Augmentative and Alternative
-              Communication Application: A Case Study.
-            </b>{" "}
-            International Conference on ICT Convergence
-          </li>
-          <li>
-            2021.10.20. 채수지, 박규동.{" "}
-            <b>
-              Design and Evaluation of Exercise Management Application for
-              Patients with Parkinsonism.
-            </b>{" "}
-            International Conference on ICT Convergence
-          </li>
-          <li>
-            2021.10.16. 정범영, 박규동.{" "}
-            <b>문자 통역 기술을 적용한 AAC 어플리케이션의 평가.</b>{" "}
-            한국보완대체의사소통학회 추계학술대회
-          </li>
-          <li>
-            2021.06.17. 채수지, 박규동.{" "}
-            <b>파킨슨병 환자의 재활을 위한 모바일 어플리케이션 설계.</b>{" "}
-            대한인간공학회 춘계학술대회
-          </li>
-          <li>
-            2021.06.17. 정범영, 박규동.{" "}
-            <b>보완대체의사소통 모바일 어플리케이션에 대한 조사.</b>{" "}
-            대한인간공학회 춘계학술대회
-          </li>
-          <li>
-            2021.06.17. 조형준, 김예원, 유찬영, 최인아, 한준아, 박규동.{" "}
-            <b>디자인 사고를 활용한 시각장애인용 프로그래밍 도구 설계.</b>{" "}
-            대한인간공학회 춘계학술대회
-          </li>
+          <ul className={styles.Lists}>
+            <li>
+              2022.04.28. 김현수, 곽소정, 정범영, 박규동.{" "}
+              <b>
+                벽걸이형 청각보조기기의 사용자 평가: 사용자 만족도 및 음질을
+                중심으로.
+              </b>{" "}
+              대한인간공학회 춘계학술대회
+            </li>
+            <li>
+              2022.02.10. 이재은, 이지은, 임예희, 박규동, 조민수.{" "}
+              <b>
+                자연어처리 기술을 활용한 유튜브 악성 댓글 자동 블라인드 시스템.
+              </b>{" "}
+              HCI Korea 2022.
+            </li>
+            <li>
+              2022.02.10. 권준혁, 소현수, 장석원, 손재성, 박규동.{" "}
+              <b>음악 커버 영상 제작을 위한 애플리케이션 설계 및 개발.</b> HCI
+              Korea 2022.
+            </li>
+            <li>
+              2021.11.29. 이영현, 김현수, 박규동.{" "}
+              <b>
+                게임화를 활용한 재활 운동의 연구 동향: 사용자의 동기부여를
+                중심으로.
+              </b>{" "}
+              대한인간공학회 추계학술대회
+            </li>
+            <li>
+              2021.10.20. 정범영, 박규동.{" "}
+              <b>
+                Heuristic Evaluation for Augmentative and Alternative
+                Communication Application: A Case Study.
+              </b>{" "}
+              International Conference on ICT Convergence
+            </li>
+            <li>
+              2021.10.20. 채수지, 박규동.{" "}
+              <b>
+                Design and Evaluation of Exercise Management Application for
+                Patients with Parkinsonism.
+              </b>{" "}
+              International Conference on ICT Convergence
+            </li>
+            <li>
+              2021.10.16. 정범영, 박규동.{" "}
+              <b>문자 통역 기술을 적용한 AAC 어플리케이션의 평가.</b>{" "}
+              한국보완대체의사소통학회 추계학술대회
+            </li>
+            <li>
+              2021.06.17. 채수지, 박규동.{" "}
+              <b>파킨슨병 환자의 재활을 위한 모바일 어플리케이션 설계.</b>{" "}
+              대한인간공학회 춘계학술대회
+            </li>
+            <li>
+              2021.06.17. 정범영, 박규동.{" "}
+              <b>보완대체의사소통 모바일 어플리케이션에 대한 조사.</b>{" "}
+              대한인간공학회 춘계학술대회
+            </li>
+            <li>
+              2021.06.17. 조형준, 김예원, 유찬영, 최인아, 한준아, 박규동.{" "}
+              <b>디자인 사고를 활용한 시각장애인용 프로그래밍 도구 설계.</b>{" "}
+              대한인간공학회 춘계학술대회
+            </li>
+          </ul>
         </ul>
       </div>
       <div className={styles.Text}>
